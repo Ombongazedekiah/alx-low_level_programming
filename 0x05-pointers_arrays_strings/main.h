a788ddd3c70400a31b185b98844349e56a6c6b65
ombonga zedekiah <zedombonga@gmail.com>
2022-10-26 16:24:23 +0300

    commit changes

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,12 @@
-
 #define MAIN_H
 
 #include <stdio.h>
 #include <unistd.h>
+
+/**
+*void prototypes()
+*int prototypes()
+*/
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -16,7 +20,8 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
-#endif 
+#endif
+
 
 
 
